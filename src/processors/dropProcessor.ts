fcac52b845032100f9c55ec2256ecf1abd9def33
Whitomtit <whitomtit@gmail.com>
2022-09-13 23:14:32 +0300

    Revert "wrap all items in parent grid component"
    This reverts commit df3fa287

--- src/processors/dropProcessor.ts
+++ src/processors/dropProcessor.ts
@@ -2,6 +2,7 @@ import { CommentParser } from 'src/comment';
 import { Options } from 'src/options';
 
 export class DropProcessor {
+	private dropRegex = /drop="[^"]+"/;
 	private slideCommentRegex = /<!--\s*(?:\.)?slide.*-->/;
 
 	private parser = new CommentParser();
@@ -15,23 +16,12 @@ export class DropProcessor {
 				return slidegroup
 					.split(new RegExp(options.verticalSeparator, 'gmi'))
 					.map(slide => {
-						if (slide.trim().length > 0) {
+						if (this.dropRegex.test(slide)) {
 							const newSlide = this.transformSlide(slide);
-							const split = output.split(slide);
-							if (split.length == 2) {
-								output = split.join(newSlide);
-
-							} else {
-								const right = split.splice(1).join(slide);
-								const left = split[0];
-								const both = [left, right];
-								output = both.join(newSlide);
-							}
+							output = output.split(slide).join(newSlide);
 							return newSlide;
-						} else {
-							return slide;
 						}
-
+						return slide;
 					})
 					.join(options.verticalSeparator);
 			})
@@ -41,39 +31,13 @@ export class DropProcessor {
 	}
 
 	transformSlide(slide: string) {
-		const [md, notes] = this.extractNotes(slide);
-
-		let outMd, outSlideComment;
-
-		if (this.slideCommentRegex.test(md)) {
-			const [match] = this.slideCommentRegex.exec(md);
+		if (this.slideCommentRegex.test(slide)) {
+			const [match] = this.slideCommentRegex.exec(slide);
 			const comment = this.parser.parseLine(match);
 			comment.addClass('drop');
-
-			outMd = md.replace(this.slideCommentRegex, '');
-			outMd = outMd.trim();
-
-			outSlideComment = this.parser.commentToString(comment);
-
+			return slide.replace(this.slideCommentRegex, this.parser.commentToString(comment));
 		} else {
-			outMd = md.trim();
-			outSlideComment = '<!-- .slide: class="drop" -->';
-		}
-
-		let out = `${outSlideComment}\n<grid absolute="true" drag="100 100" drop="0 0">\n${outMd}\n</grid>`;
-
-		if (notes.length > 0) {
-			out += '\n\n' + notes;
-		}
-		return out;
-	}
-
-	extractNotes(input: string): [string, string] {
-		const spliceIdx = input.indexOf('note:');
-		if (spliceIdx > 0) {
-			return [input.substring(0, spliceIdx), input.substring(spliceIdx)];
-		} else {
-			return [input, ''];
+			return slide + '\n<!-- .slide: class="drop" -->\n';
 		}
 	}
 }
