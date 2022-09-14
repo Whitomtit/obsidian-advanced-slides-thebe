d92ac2cd44804e20ed4c04a9dd6c68d77e476517
Whitomtit <whitomtit@gmail.com>
2022-09-14 17:46:27 +0300

    Cursor scaling fix

--- src/plugin_thebe.js
+++ src/plugin_thebe.js
@@ -12,6 +12,22 @@ CodeMirror.defineOption("blurOnExecute", false, function (cm, val) {
 		}
 	});
 });
+let element = document.createElement('style');
+document.head.appendChild(element);
+let cursorSheet = element.sheet;
+
+Reveal.on( 'resize', event => {
+	if (cursorSheet.cssRules.length > 0)
+		cursorSheet.deleteRule(0);
+
+	cursorSheet.insertRule(
+		`.CodeMirror-cursors,
+		.CodeMirror-measure:nth-child(2) + div {
+				transform: scale(${1 / event.scale});
+		 		transform-origin: 0 0;
+		}`,
+		0);
+});
 
 function thebe_init(kernel, host) {
 	thebelab.bootstrap({
